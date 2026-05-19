aac4119cc8f0e072e05e3ffd97e3066be144cda4
AeroGenCreator <gallo85floyd@gmail.com>
2026-05-19 07:14:08 -0600

    [REF] NEW QUERY-BOX 1ST TRY

---
+++ pancakes/abstract/querybox.py
@@ -0,0 +1,422 @@
+"""
+Declaracion De Clase QueryBox() Queries declarativos a traves
+de **kwargs y encadenamiento de metodos.
+"""
+
+# Modulos Propios
+import logging
+
+from ..orm.query import query
+from ..sql.datatype import ForeignKey
+from ..tools.functions import environment
+
+# .envs; log, dir, db
+envs = environment()
+LOG = envs.get("log", "WARNING")
+
+log_level = getattr(logging, LOG, logging.WARNING)
+logging.basicConfig(
+    level=log_level,
+    format="%(asctime)s [%(levelname)s] "
+    "%(name)s.%(funcName)s:%(lineno)d - %(message)s",
+    force=True,
+)
+logger = logging.getLogger(__name__)
+
+
+class QueryBox:
+    # Pasar MODELO "instanciado" es obligatorio
+    def __init__(self, model):
+        self.model = model
+        self.reset()
+
+    def reset(self):
+        self.DC_LABEL = {}
+        self.SE_LABEL = []
+        self.SP_LABEL = []
+        self.SE_SELECT = []
+        self.SP_SELECT = []
+        self.FROM = None
+        self.JOIN = None
+        self.CONDITION = None
+        self.GROUP = None
+        self.ORDER = None
+        self.LIMIT = None
+        self.OFF = None
+        self.IDS = False
+        self.ROW = None
+        self.COL = None
+
+    def select(self, *select):
+        """
+        Sintaxis de seleccion:
+
+        1. 'tabla__columna', ...
+        2. 'tabla__columna__Agregacion', ...
+
+        Agregaciones validas:
+
+        {
+            "min": "MIN",
+            "max": "MAX",
+            "sum": "SUM",
+            "count": "COUNT",
+            "avg": "AVG",
+            "": "",
+        }
+        """
+
+        # Agregaciones validas
+        AGGS = {
+            "min": "MIN",
+            "max": "MAX",
+            "sum": "SUM",
+            "count": "COUNT",
+            "avg": "AVG",
+            "": "",
+        }
+
+        # Modelo
+        MODEL = self.model
+
+        # Validar select:
+        if not select:
+            return self
+
+        # Tablas en base de datos
+        DB_TABLES = list(MODEL._family.keys())
+        ACTUAL_TB = MODEL._table
+
+        # Iterar select
+        for col in select:
+            col = col.lower()
+
+            # Validar estructura de select
+            validate = (not isinstance(col, str), ("__" not in col))
+            if any(validate):
+                logger.critical(
+                    "Invalid syntax passed in selection. "
+                    "To declara a selection you must specify: "
+                    "table__column__aggregation. "
+                    f"Error found in {col}"
+                )
+                raise ValueError
+
+            # Partir select en segmentos
+            PARTS = col.split("__")
+
+            # Validar cantidad de partes
+            if len(PARTS) not in (2, 3):
+                logger.critical(
+                    "Invalid quantity of arguments passed in select. "
+                    "You must separate arguments with '__'. "
+                    "Syntax: TABLE__COLUMN__AGGREGATION or "
+                    "TABLE__COLUMN"
+                )
+                raise ValueError
+
+            # Extraer y validar agregacion, sino ""
+            AGG = ""
+            if len(PARTS) == 3:
+                AGG = PARTS[2]
+                if AGG not in AGGS:
+                    logger.critical(
+                        "Invalid aggregation function passed in select. "
+                        f"Column: {col}, agregation: {AGG}"
+                    )
+                    raise ValueError
+                AGG = AGGS[AGG]
+
+            # Extraccion de TABLA y COLUMNA
+            TAB = PARTS[0]
+            COL = PARTS[1]
+
+            # SELECCION SIMPLE
+            if TAB == ACTUAL_TB:
+                # Obtener etiquetas frontend
+                MAPPED = dict(
+                    zip(
+                        MODEL._family[TAB]._metadata[TAB]["columns"],
+                        MODEL._family[TAB]._metadata[TAB]["comments"],
+                    )
+                )
+                LAB = MAPPED[COL]
+                self.SE_LABEL.append(" ".join(f"{LAB} {AGG.upper()}".split()))
+
+                # Seleccion simple resultado
+                self.SE_SELECT.append({"name": COL, "agg": AGG})
+                continue
+
+            # SELECCION ESPECIAL
+            if TAB in DB_TABLES:
+                # Obtener etiquetas frontend
+                MAPPED = dict(
+                    zip(
+                        MODEL._family[TAB]._metadata[TAB]["columns"],
+                        MODEL._family[TAB]._metadata[TAB]["comments"],
+                    )
+                )
+                LAB = MAPPED[COL]
+                self.SP_LABEL.append(" ".join(f"{LAB} {AGG.upper()}".split()))
+
+                # Seleccion especial resultado
+                self.SP_SELECT.append({"table": TAB, "name": COL, "agg": AGG})
+
+            # TAB no existe en la base de datos
+            else:
+                logger.critical(
+                    f"The following table: {TAB} does not exist in Database. "
+                    "Make sure passed arguments exist and proper syntax "
+                    "using '__' as separator."
+                )
+                raise ValueError
+
+        # Validar: NO SE_SELECT, SI SP_SELECT
+        if not self.SE_SELECT:
+            FIRST_COL = MODEL._family[ACTUAL_TB]._metadata[ACTUAL_TB][
+                "columns"
+            ][0]
+            self.SE_SELECT.append({"name": FIRST_COL, "agg": ""})
+
+        return self
+
+    def _NO_SELECT_(self) -> None:
+
+        MODEL = self.model
+        MAINT = self.model._table
+
+        # OBTENER TODAS LAS ETIQUETAS Y COLUMNAS SELECT SIMPLE
+        SE_MAPPED = dict(
+            zip(
+                MODEL._family[MAINT]._metadata[MAINT]["columns"],
+                MODEL._family[MAINT]._metadata[MAINT]["comments"],
+            )
+        )
+
+        COLUMNS = list(SE_MAPPED.keys())
+        COMMENTS = list(SE_MAPPED.values())
+
+        self.SE_LABEL.extend(COMMENTS)
+
+        for COL in COLUMNS:
+            self.SE_SELECT.append({"name": COL})
+
+        # OBTENER TODO DE UN JOIN
+        CACHE = []
+        if self.JOIN:
+            # TABLAS EXTRAS EN ARGUMENTOS DE JOIN
+            for join in self.JOIN:
+                TAB1 = join.get("tab1", "")
+                TAB2 = join.get("tab2", "")
+
+                if TAB1 not in CACHE and TAB1 != MAINT:
+                    CACHE.append(TAB1)
+
+                    CHART1 = dict(
+                        zip(
+                            MODEL._family[TAB1]._metadata[TAB1]["columns"],
+                            MODEL._family[TAB1]._metadata[TAB1]["comments"],
+                        )
+                    )
+
+                    COLS1 = list(CHART1.keys())
+                    LABS1 = list(CHART1.values())
+
+                    listado1 = []
+                    for COL in COLS1:
+                        listado1.append({"table": TAB1, "name": COL})
+
+                    self.SP_LABEL.extend(LABS1)
+                    self.SP_SELECT.extend(listado1)
+
+                if TAB2 not in CACHE and TAB2 != MAINT:
+                    CACHE.append(TAB2)
+
+                    CHART2 = dict(
+                        zip(
+                            MODEL._family[TAB2]._metadata[TAB2]["columns"],
+                            MODEL._family[TAB2]._metadata[TAB2]["comments"],
+                        )
+                    )
+
+                    COLS2 = list(CHART2.keys())
+                    LABS2 = list(CHART2.values())
+
+                    listado2 = []
+                    for COL in COLS2:
+                        listado2.append({"table": TAB2, "name": COL})
+
+                    self.SP_LABEL.extend(LABS2)
+                    self.SP_SELECT.extend(listado2)
+
+    def link(self, *unions):
+        """
+        SINTAXIS:
+
+        'UNION__TABLE', ...
+
+        UNIONES VALIDAS:
+
+        in = INNER
+        rg = RIGHT
+        lf = LEFT
+        """
+
+        # UNIONES VALIDAS
+        JOINS = {"in": "INNER", "rg": "RIGHT", "lf": "LEFT"}
+
+        MODEL = self.model
+        MAINT = self.model._table
+
+        # CACHEAR TABLAS, LINKERS y DEPENDENCIAS
+        TABLAS = [MAINT]
+        LINKERS = ["in"]
+        DEPENDENCIES = [MODEL._family[MAINT]._metadata[MAINT]["depends"]]
+
+        # ITERACION DE UNIONES
+        for union in unions:
+            union = union.lower()
+
+            # Validar oracion de union
+            validate = (not isinstance(union, str), ("__" not in union))
+            if any(validate):
+                logger.critical(
+                    "Invalid syntax, every union declaration depends "
+                    "on specifying union nature and separator '__' "
+                    "', example: in__table; INNER JOIN table. "
+                    "Valid datatype: Only strings."
+                )
+                raise ValueError
+
+            # Extraccion de datos de la union
+            PARTS = union.split("__")
+
+            LINKER = PARTS[0]
+            EXTRAT = PARTS[1]
+
+            # Validar LINKER
+            if LINKER not in list(JOINS.keys()):
+                logger.critical(
+                    f"Invalid type of join: {LINKER}. Valid ones are {JOINS}."
+                )
+                raise ValueError
+            # Validar que exista la tabla
+            DB_TABLES = list(MODEL._family.keys())
+            if EXTRAT not in DB_TABLES:
+                logger.critical(
+                    "Passed table does not exist in DATABASE. "
+                    f"Passed tables: {EXTRAT}. "
+                    f"DATABSE tables: {DB_TABLES}"
+                )
+
+            if EXTRAT in TABLAS:
+                logger.critical(
+                    "Duplicated table passed. Your model is itself a table. "
+                    "Any repeated table in your sentence will raise this error."
+                )
+                raise ValueError
+
+            TABLAS.append(EXTRAT)
+            LINKERS.append(LINKER)
+            DEPENDENCIES.append(
+                MODEL._family[EXTRAT]._metadata[EXTRAT]["depends"]
+            )
+
+        # MAP TABLAS Y SUS DEPENDENCIAS Y UNIONES
+        MAP_DEPN = [{t: d} for t, d in zip(TABLAS, DEPENDENCIES)]
+        MAP_LINK = {}
+        for t, link in zip(TABLAS, LINKERS):
+            MAP_LINK.update({t: link})
+
+        # CORREGIR LOOP
+        ORDEN = []
+        COPIA = MAP_DEPN.copy()
+        while len(COPIA) > 0:
+            for index, dicc in enumerate(COPIA):
+                tupla = list(dicc.items())[0]
+                tabla = tupla[0]
+                depen = tupla[1]
+
+                try:
+                    tupla2 = list(COPIA[index + 1].items())[0]
+                    tabla2 = tupla2[0]
+                    depen2 = tupla2[1]
+
+                    if tabla == depen2[0]:
+                        ORDEN.append(tabla)
+                        COPIA.pop(index + 1)
+
+                    elif depen[0] == tabla2:
+                        ORDEN.append(tabla2)
+                        COPIA.pop(index)
+                except Exception:
+                    pass
+
+                finally:
+                    if depen == ["self"]:
+                        ORDEN.append(tabla)
+                        COPIA.pop(index)
+
+                    elif set(depen).issubset(set(ORDEN)):
+                        ORDEN.append(tabla)
+                        COPIA.pop(index)
+
+        RESULT = []
+        PADRES = ORDEN[0]
+        for TABLA in ORDEN:
+
+            CAMPOS = MODEL._family[TABLA]._metadata[TABLA]["fields"]
+
+            for CAMP in CAMPOS:
+                if isinstance(CAMP, ForeignKey):
+                    THIJA = TABLA
+                    COL_HIJA = CAMP._name
+                    SECOND_TABLE=getattr(CAMP, "second_table")
+                    APUNTA_A=getattr(CAMP, "column_id")
+
+                    if SECOND_TABLE == PADRES:
+                        self.FROM = SECOND_TABLE
+
+                    RESULT.append(
+                        {
+                        "join": JOINS[MAP_LINK[SECOND_TABLE]],
+                        "tab1": THIJA,
+                        "id1": COL_HIJA,
+                        "tab2": SECOND_TABLE,
+                        "id2": APUNTA_A
+                        }
+                    )
+
+        self.JOIN = RESULT
+        return self
+
+    def all(self):
+
+        # REASIGNAR VALOR FROM -> SEGUN JOINS
+        if self.FROM:
+            FROM = self.FROM
+            self.model = self.model._family[self.FROM]
+            self._NO_SELECT_()
+        else:
+            FROM = self.model._table
+
+        # VALIDAR NO SELECCION
+        if not self.SE_SELECT and not self.SP_SELECT:
+            self._NO_SELECT_()
+
+        # Obtencion de argumentos
+        PATH = self.model._db_file
+        SPECIAL = None if not self.SP_SELECT else self.SP_SELECT
+        JOIN = None if not self.JOIN else self.JOIN
+
+        row, col = query(
+            db_path=PATH,
+            select=self.SE_SELECT,
+            _from=FROM,
+            sp_select=SPECIAL,
+            join=JOIN
+        )
+
+        self.reset()
+
+        return row, col
---
+++ tests/test_class_querybox_renewed.py
@@ -0,0 +1,183 @@
+# Copyright 2026 AeroGenCreator
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+"""Test QueryBox Class -*- PanCakesORM -*-"""
+
+# Modulos Propios
+# Modulos Python
+from pathlib import Path
+
+from pancakes.abstract.querybox import QueryBox
+from pancakes.models.model import PanCakesORM
+from pancakes.orm.insert import insert
+from pancakes.sql import datatype
+
+# GLOBAL PATH para pruebas
+dir_ = Path.cwd() / "data" / "test_env"
+file = dir_ / "querybox_renewed.sqlite"
+
+
+class Country(PanCakesORM):
+    _db_dir = dir_
+    _db_file = file
+
+    _table = "country"
+    _depends = "self"
+
+    name = datatype.Char(comment="Country")
+
+
+class Client(PanCakesORM):
+    _db_dir = dir_
+    _db_file = file
+
+    _table = "client"
+    _depends = ["country"]
+
+    name = datatype.Char(comment="Client Name")
+    country_id = datatype.ForeignKey(
+        second_table="country",
+        column_id="country_id",
+        comment="Country Rel",
+        on_del="cascade",
+        on_upd="cascade",
+    )
+
+
+class Sale(PanCakesORM):
+    _db_dir = dir_
+    _db_file = file
+
+    _table = "sale"
+    _depends = ["client"]
+
+    name = datatype.Char(comment="Sale Code")
+    client_id = datatype.ForeignKey(
+        second_table="client",
+        column_id="client_id",
+        comment="Cliente Rel",
+        on_del="cascade",
+        on_upd="cascade",
+    )
+
+
+insert(
+    db_path=file,
+    params=[
+        {"table": "country", "data": [(None, "Mexico"), (None, "Brasil")]},
+        {
+            "table": "client",
+            "data": [
+                (None, "Andres", 1),
+                (None, "Lupita", 1),
+                (None, "Peke", 2),
+                (None, "Polar", 1),
+                (None, "Malteada", 2),
+            ],
+        },
+        {
+            "table": "sale",
+            "data": [
+                (None, "F1", 1),
+                (None, "F2", 3),
+                (None, "F3", 4),
+                (None, "F4", 1),
+                (None, "F5", 3),
+                (None, "F6", 3),
+                (None, "F7", 5),
+                (None, "F8", 3),
+                (None, "F9", 2),
+            ],
+        },
+    ],
+)
+
+q_country = QueryBox(Country)
+q_client = QueryBox(Client)
+q_sale = QueryBox(Sale)
+
+""" PRUEBAS CON EL NUEVO MOTOR DE QUERY DECLARATIVO """
+
+
+def test_select_uno():
+    row, col = q_country.select("country__country_id").all()
+
+    assert row == [(1,), (2,)]
+    assert col == ["country__country_id"]
+
+
+def test_select_varios():
+    row, col = q_country.select("country__country_id", "country__name").all()
+
+    assert row == [(1, "Mexico"), (2, "Brasil")]
+    assert col == ["country__country_id", "country__name"]
+
+
+def test_select_uno_agg():
+    row, col = q_client.select("client__client_id__count").all()
+
+    assert row == [(5,)]
+    assert col == ["client__client_id__count"]
+
+
+def test_select_varios_agg():
+    row, col = q_client.select(
+        "client__client_id__count", "client__client_id__sum"
+    ).all()
+
+    assert row == [(5, 15)]
+    assert col == ["client__client_id__count", "client__client_id__sum"]
+
+
+def test_no_select():
+    row, col = q_sale.select().all()
+
+    assert row == [
+        (1, "F1", 1),
+        (2, "F2", 3),
+        (3, "F3", 4),
+        (4, "F4", 1),
+        (5, "F5", 3),
+        (6, "F6", 3),
+        (7, "F7", 5),
+        (8, "F8", 3),
+        (9, "F9", 2),
+    ]
+    assert col == ["sale__sale_id", "sale__name", "sale__client_id"]
+
+
+def test_no_select_link_3():
+    row, col = q_sale.link("lf__client", "lf__country").all()
+
+    assert row == [
+        (1, "Mexico", 1, "Andres", 1, 1, "F1", 1),
+        (1, "Mexico", 1, "Andres", 1, 4, "F4", 1),
+        (1, "Mexico", 2, "Lupita", 1, 9, "F9", 2),
+        (1, "Mexico", 4, "Polar", 1, 3, "F3", 4),
+        (2, "Brasil", 5, "Malteada", 2, 7, "F7", 5),
+        (2, "Brasil", 3, "Peke", 2, 2, "F2", 3),
+        (2, "Brasil", 3, "Peke", 2, 5, "F5", 3),
+        (2, "Brasil", 3, "Peke", 2, 6, "F6", 3),
+        (2, "Brasil", 3, "Peke", 2, 8, "F8", 3),
+    ]
+
+    assert col == [
+        "country__country_id",
+        "country__name",
+        "client__client_id",
+        "client__name",
+        "client__country_id",
+        "sale__sale_id",
+        "sale__name",
+        "sale__client_id",
+    ]
+
+
+def test_no_select_link_2():
+    row, col = q_client.link("lf__sale").all()
+
+    print(row)
+    print(col)
